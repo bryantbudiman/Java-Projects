607f50ec60630cf4e7ba00ef851bb2f5cca9b45d
Benjamin Bryant Budiman <bbbudima@usc.edu>
2017-11-20 13:21:00 -0800

    Update and rename <bbbudima>-a4.sql to jeopardy.sql

--- Jeopardy!/jeopardy.sql
+++ Jeopardy!/jeopardy.sql
@@ -1,6 +1,6 @@
-drop database if exists bbbudimaA4;
-create database bbbudimaA4; 
-use bbbudimaA4; 
+drop database if exists jeopardy;
+create database jeopardy; 
+use jeopardy; 
 
 create table loginInformation (
    username varchar(50) not null primary key, 
